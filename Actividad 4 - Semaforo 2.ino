114fb8796b2d6c11260749f97fd42aa77a6986e5
AndersonCepedaSandoval1 <148785026+AndersonCepedaSandoval1@users.noreply.github.com>
2023-10-28 20:00:41 -0500

    Update Actividad 4 - Semaforo 2.ino

--- Actividad 4 - Semaforo 2.ino
+++ Actividad 4 - Semaforo 2.ino
@@ -1,7 +1,8 @@
 // Microcontroladores - 2674489
-// Actividad 2
+// Actividad 4
 // Anderson Cepeda Sandoval y Juan Sebastian Guanumen
 
+long valor;
 int var1 = 11;   
 int var2 = 10;
 int var4 = A0;
@@ -11,6 +12,8 @@ int led1 = 6;
 int led2 = 5;  
 
 int contador = 0;
+int sensor;
+
 void setup() {
   pinMode(var1, INPUT);     
   pinMode(var2, INPUT);
@@ -24,46 +27,52 @@ void setup() {
 }
 
 void loop()
-
 { 
+ 
+  sensor = analogRead(A0);
     
   if (digitalRead(11) == HIGH && digitalRead(10)== LOW){ 
     
-  digitalWrite(led, HIGH);
-  delay (analogRead(A0));
-  digitalWrite(led, LOW);
-    
+     digitalWrite(led, HIGH);
+    delay (5000/sensor);
+    digitalWrite(led, LOW);
+
+	Serial.print("El led a paso por HIGH: ");
+  
   contador = led + contador++ - 6;
   Serial.print(contador);
-    delay (analogRead(A0));
-    
-  digitalWrite(led, HIGH);
-  digitalWrite(led1, HIGH);
-  delay (analogRead(A0));
-  digitalWrite(led, LOW);
-  digitalWrite(led1, LOW);
-    
-  digitalWrite(led2, HIGH);
-  delay (analogRead(A0));
-  digitalWrite(led2, LOW);
-    
-  digitalWrite(led1, HIGH);
-  delay (analogRead(A0));
-   digitalWrite(led1, LOW);
-    
-    Serial.print(analogRead(A0));
-   delay (500);
- } 
+    delay (sensor);
   
+ Serial.println("  ");
+  
+  Serial.print("El valor del potencimetro es = ");
+  Serial.println(sensor);
+    
+    digitalWrite(led, HIGH);
+    digitalWrite(led1, HIGH);
+    delay (1000/sensor);
+    digitalWrite(led, LOW);
+    digitalWrite(led1, LOW);
+
+    digitalWrite(led2, HIGH);
+    delay (3000/sensor);
+    digitalWrite(led2, LOW);
+
+    digitalWrite(led1, HIGH);
+    delay (1000/sensor);
+     digitalWrite(led1, LOW);
+    } 
+
   if (digitalRead(11)== LOW && digitalRead(10)== HIGH){ 
    digitalWrite(led1, HIGH);
-   delay (analogRead(A0));
+   delay (500/sensor);
    digitalWrite(led1, LOW);
-   delay (analogRead(A0));
+   delay (500/sensor);
     
-   Serial.print(analogRead(A0));
-   delay (500);
- 
+   Serial.print("El valor del potencimetro es = "); 
+   Serial.println(sensor);
+   delay (500/sensor);
+
   }
   
   if (digitalRead(11)== HIGH && digitalRead(13)== HIGH){ 
